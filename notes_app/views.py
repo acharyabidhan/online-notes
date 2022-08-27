ad6b85799532c0196d1e1fd89d6b8316f6cebc37
acharyabidhan <acharyaumesh2005@gmail.com>
2022-08-27 08:58:26 +0545

    some settings changed

--- notes_app/views.py
+++ notes_app/views.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DB_PATH = "DataStorage"
 
 def get_fav_icon():
-    with open(f"{BASE_DIR}\\favicon.png", "rb") as fi:
+    with open(f"{BASE_DIR}//favicon.png", "rb") as fi:
         icon_data = fi.read()
         b64_icon_data = base64.b64encode(icon_data)
         base64_icon_data = b64_icon_data.decode("UTF-8")
@@ -30,19 +30,19 @@ public_notes_html = """
 """
 
 def get_public_notes():
-    public_notes_path = f"{BASE_DIR}\\public"
+    public_notes_path = f"{BASE_DIR}//public"
     public_notes_list = os.listdir(public_notes_path)
     public_notes_list.remove("blank")
     if public_notes_list:
         all_public_notes = []
         for uname in public_notes_list:
-            one_user_public_notes_list = os.listdir(f"{public_notes_path}\\{uname}")
+            one_user_public_notes_list = os.listdir(f"{public_notes_path}//{uname}")
             if one_user_public_notes_list:
                 for public_notes in one_user_public_notes_list:
                     one_public_note = []
                     one_public_note.append(uname)
                     one_public_note.append(public_notes)
-                    with open(f"{public_notes_path}\\{uname}\\{public_notes}\\note.n", "rb") as pn:
+                    with open(f"{public_notes_path}//{uname}//{public_notes}//note.n", "rb") as pn:
                         act_public_note = pn.read()
                         one_public_note.append(act_public_note.decode())
                     all_public_notes.append(one_public_note)
@@ -67,7 +67,7 @@ def render_login_page(request):
 
 def get_registered_user_data(user):
     try:
-        with open(f"{BASE_DIR}\\{DB_PATH}\\{user}\\account.json", "r") as ud:
+        with open(f"{BASE_DIR}//{DB_PATH}//{user}//account.json", "r") as ud:
             user_data = ud.read()
             return user_data
     except Exception as e:
@@ -91,7 +91,7 @@ NOTE_HTML = """
 """
 
 def get_all_notes(user):
-    all_notes_path = f"{BASE_DIR}\\{DB_PATH}\\{user}"
+    all_notes_path = f"{BASE_DIR}//{DB_PATH}//{user}"
     all_notes_list = os.listdir(all_notes_path)
     all_notes_list.remove("account.json")
     all_notes_list.reverse()
@@ -101,7 +101,7 @@ def get_all_notes(user):
             one_note = []
             one_note.append(str(index))
             one_note.append(data)
-            with open(f"{all_notes_path}\\{data}\\note.json", "r") as nte:
+            with open(f"{all_notes_path}//{data}//note.json", "r") as nte:
                 note_data = json.loads(nte.read())
                 one_note.append(note_data["pp"])
                 one_note.append(note_data["note"])
@@ -129,8 +129,8 @@ def delete_notes(request):
     key2del = request.POST["key2del"]
     new_porp = str(porp).split()
     p_or_p = new_porp[-1].lower()
-    private_delete_path = f"{BASE_DIR}\\{DB_PATH}\\{uname}\\{key2del}"
-    public_delete_path = f"{BASE_DIR}\\public\\{uname}\\{key2del}"
+    private_delete_path = f"{BASE_DIR}//{DB_PATH}//{uname}//{key2del}"
+    public_delete_path = f"{BASE_DIR}//public//{uname}//{key2del}"
     all_delete_paths = [private_delete_path, public_delete_path]
     if json.loads(get_registered_user_data(uname))["password"] == psw:
         if p_or_p == "private":
@@ -165,7 +165,7 @@ def check_login(request):
 
 def edit_account_json(*args):
     uname, mode, data = args
-    with open(f"{BASE_DIR}\\{DB_PATH}\\{uname}\\account.json", mode) as acc:
+    with open(f"{BASE_DIR}//{DB_PATH}//{uname}//account.json", mode) as acc:
         if mode == "r":
             return acc.read()
         elif mode == "w":
@@ -216,7 +216,7 @@ def render_signup_page(request):
     return render(request, "signup.html", {"favicon": get_fav_icon()})
 
 def check_user_existence(user):
-    if os.path.isdir(f"{BASE_DIR}\\{DB_PATH}\\{user}"):
+    if os.path.isdir(f"{BASE_DIR}//{DB_PATH}//{user}"):
         return True
     else:
         return False
@@ -224,7 +224,7 @@ def check_user_existence(user):
 def save_new_user_data(path, data):
     try:
         os.mkdir(path)
-        with open(f"{path}\\account.json", "w") as ud:
+        with open(f"{path}//account.json", "w") as ud:
             ud.write(data)
         return True
     except Exception as e:
@@ -248,7 +248,7 @@ def check_signup(request):
             "answer2": answer2,
         })
         if not check_user_existence(username):
-            new_user_path = f"{BASE_DIR}\\{DB_PATH}\\{username}"
+            new_user_path = f"{BASE_DIR}//{DB_PATH}//{username}"
             if save_new_user_data(new_user_path, signup_data):
                 return JsonResponse({"status": "signup_success"}, safe=False)
             else:
@@ -265,9 +265,9 @@ def save_notes_private(fdata):
     json_data = json.loads(dict_dta)
     p_u_name = json_data["uname"]
     p_date = json_data["date"]
-    u_path = f"{BASE_DIR}\\{DB_PATH}\\{p_u_name}\\{p_date}"
+    u_path = f"{BASE_DIR}//{DB_PATH}//{p_u_name}//{p_date}"
     os.mkdir(u_path)
-    with open(f"{u_path}\\note.json", "w") as sn:
+    with open(f"{u_path}//note.json", "w") as sn:
         sn.write(dict_dta)
 
 def save_notes_public(fdata):
@@ -287,8 +287,8 @@ def save_notes_public(fdata):
         "date": ddd_again
     }
     save_notes_private(for_private_notes)
-    p_path = f"{BASE_DIR}\\public\\{u_name}\\"
-    new_path = f"{p_path}{ddd_again}\\"
+    p_path = f"{BASE_DIR}//public//{u_name}//"
+    new_path = f"{p_path}{ddd_again}//"
     if os.path.isdir(p_path) == False:
         os.mkdir(p_path)
         os.mkdir(new_path)
